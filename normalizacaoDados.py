3cbc768b9ddc470f1d9d9f4e7635b699d02d14f9
HeitorDalla <heitorvillavilla@gmail.com>
2025-07-01 09:45:07 -0300

    Finalizado o tratamento dos dados utilizando o pandas

--- normalizacaoDados.py
+++ normalizacaoDados.py
@@ -1,9 +1,22 @@
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split # embaralhar os dados
+from sklearn.model_selection import train_test_split # divide os dados em treinamento e teste
 
-df = pd.read_csv("data/car-sales-extended.csv")
+df = pd.read_csv("data/car-sales-extended-missing-data.csv")
 
+# Tratamento de dados
+print(df.isna().sum()) # verifica quantos valores ausentes há em cada coluna
+
+# 1 Maneira - Preencher com valores lógicos para interferir o menos possível
+print(df.info())
+
+df['Make'] = df['Make'].fillna('missing') # preenche todos os valores nulos com 'missing'
+df['Colour'] = df['Colour'].fillna('missing') # preenche todos os valores nulos com 'missing'
+df['Odometer (KM)'] = df['Odometer (KM)'].fillna(df['Odometer (KM)'].mean()) # preenche todos os valores nulos com a média da coluna
+df['Doors'] = df['Doors'].fillna(4) # preenche todos os valores nulos com o maior que aparece (4)
+df = df.dropna() # o melhor a se fazer é excluir os dados faltantes, pois não temos base de previsão para a coluna alvo
+
+# Separo as variáveis independentes da variável alvo
 X = df.drop('Price', axis=1)
 y = df['Price']
 
@@ -12,16 +25,17 @@ print(df.info())
 
 # Transformar as categorias em números
 from sklearn.preprocessing import OneHotEncoder # transformar categorias em números
-from sklearn.compose import ColumnTransformer
+from sklearn.compose import ColumnTransformer # aplica essa transformação nas colunas escolhidas
 
 variaveis_categoricas = ['Make', 'Colour', 'Doors'] # Doors entra por ser uma variável categorizadora numérica
-one_hot = OneHotEncoder()
-transformer = ColumnTransformer([('one_hot',
-                                 one_hot,
-                                 variaveis_categoricas)],
-                                 remainder='passthrough')
 
-transformed_X = transformer.fit_transform(X)
+one_hot = OneHotEncoder() # cria um codificador de texto para números binários, criando uma coluna com 0 ou 1
+transformer = ColumnTransformer([('one_hot', # nome da transformação
+                                 one_hot, 
+                                 variaveis_categoricas)], # lista como o nome das variáveis categóricas
+                                 remainder='passthrough') # define o que fazer com as colunas que não estão nas lista de transformação, no caso, mantém no conjunto de dados
+
+transformed_X = transformer.fit_transform(X) # analisa os dados e faz a codificação
 
 X_train, X_test, y_train, y_test = train_test_split(transformed_X, # colocando o dataframe numérico apenas
                                                     y, 
