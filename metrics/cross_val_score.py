4a0a4291b8a0a39e273837d966096168a8c09455
HeitorDalla <heitorvillavilla@gmail.com>
2025-07-02 20:52:08 -0300

    Modelo para avaliação dos dados de testes

--- metrics/cross_val_score.py
+++ metrics/cross_val_score.py
@@ -1,12 +1,11 @@
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
 from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
 # Modelos de treinamento
-from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
 
-np.random.seed(42)
+# np.random.seed(42)
 
 carregamento = load_iris()
 
@@ -28,15 +27,22 @@ X_train, X_test, y_train, y_test = train_test_split(X,
 # Instanciando o modelo de classificacao
 model = RandomForestClassifier()
 
- # Treinamento dos dados
+# Treinamento dos dados
 training = model.fit(X_train, y_train)
 
 # Predict dos dados
 y_preds = model.predict(X_test)
 
 # Avaliação do modelo
-from sklearn.metrics import accuracy_score
+from sklearn.model_selection import cross_val_score
 
 # Checar a acurácia do modelo, ou seja, porcentagem de acertos em cima dos exemplos
-avaliacao = accuracy_score(y_test, y_preds)
-print(avaliacao)
+default = cross_val_score(model, X, y, cv=10, scoring=None) # cv - quantidade que eu quero treinar
+
+# Média do modelo 'cross_val_score'
+print(np.mean(default))
+print('-----------------')
+
+# Comparação da classificação cruzada X score normal
+print(model.score(X_test, y_test))
+print(np.mean(default))
