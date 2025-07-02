6df09ad6bdc5cedb4783ee85266dc54609c7e9f8
HeitorDalla <heitorvillavilla@gmail.com>
2025-07-02 20:51:27 -0300

    Organizando as informacoes sobre as metricas utilizadas

---
+++ models_training/classification.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import numpy as np
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+# Modelos de treinamento
+from sklearn.ensemble import RandomForestClassifier
+
+np.random.seed(42)
+
+carregamento = load_iris()
+
+dataframe = pd.DataFrame(carregamento['data'], columns=carregamento['feature_names'])
+
+dataframe['target'] = carregamento['target']
+
+print(dataframe)
+
+# Separar os dados em variáveis dependentes e independentes
+X = dataframe.drop('target', axis=1)
+y = dataframe['target']
+
+# Separar os dados em treinamento e teste
+X_train, X_test, y_train, y_test = train_test_split(X,
+                                                    y,
+                                                    test_size=0.2)
+
+# Instanciando o modelo de classificacao
+model = RandomForestClassifier()
+
+# Treinamento dos dados
+training = model.fit(X_train, y_train)
+
+# Predict dos dados
+y_preds = model.predict(X_test)
+
+# Avaliação do modelo
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import accuracy_score # Checar a acurácia do modelo, ou seja, porcentagem de acertos em cima dos exemplos
+
+default_cross_score = cross_val_score(model, X, y, cv=10, scoring=None) # array de 10 treinamentos diferentes
+
+accuracy_cross_score = cross_val_score(model, X, y, cv=10, scoring='accuracy') # array de 10 treinamentos diferentes
+
+test_score = accuracy_score(y_test, y_preds) # valor de um treinamento
+
+# São equivalentes ao mesmo resultado.
---
+++ models_training/regression.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import numpy as np
+from sklearn.datasets import fetch_california_housing
+from sklearn.model_selection import train_test_split
+# Modelos de regressão
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import Ridge
+from sklearn.linear_model import Lasso
+from sklearn.svm import SVR
+
+# Tratamento dos Dados
+# Análise dos Dados
+housing = fetch_california_housing()
+
+dataframe = pd.DataFrame(housing['data'], columns=housing['feature_names'])
+
+dataframe['target'] = housing['target']
+
+# Separar os dados em variáveis dependentes e independentes
+X = dataframe.drop('target', axis=1)
+y = dataframe['target']
+
+# Importar algoritmo de modelo para o aprendizado de máquina
+np.random.seed(42)
+
+# Dividir os dados em treinamento e teste
+X_train, X_test, y_train, y_test = train_test_split(X,
+                                                     y,
+                                                     test_size=0.2)
+
+# Instanciar o modelo e ajusta-lo
+model = RandomForestRegressor()
+
+# Treinar o modelo
+training = model.fit(X_train, y_train)
+
+# Fazer uma previsão
+y_preds_test = model.predict(X_test)
+
+# Checar score do modelo com o conjunto de teste
+from sklearn.model_selection import cross_val_score # Vai gerar 'cv' treinamentos diferentes para o mesmo conjunto de dados
+from sklearn.metrics import r2_score # Indica quanto da variação dos valores reais o modelo de regressão conseguiu explicar
+
+default_cross_score = cross_val_score(model, X, y, cv=10, scoring=None) # array de 10 treinamentos diferentes
+
+r2_cross_score = cross_val_score(model, X, y, cv=10, scoring='r2') # array de 10 treinamentos diferentes
+
+test_score = r2_score(y_test, y_preds_test) # valor de um treinamento
+
+# São equivalentes ao mesmo resultado.
