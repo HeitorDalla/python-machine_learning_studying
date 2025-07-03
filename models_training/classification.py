326cad2c192d4b99313b731a4d669e5abee4043f
HeitorDalla <heitorvillavilla@gmail.com>
2025-07-03 15:18:15 -0300

    Organizando as metricas para o modelo de classificacao

--- models_training/classification.py
+++ models_training/classification.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 # Modelos de treinamento
@@ -24,24 +25,71 @@ X_train, X_test, y_train, y_test = train_test_split(X,
                                                     y,
                                                     test_size=0.2)
 
-# Instanciando o modelo de classificacao
+
+# Instanciando o modelo de classificacao e fazendo previsões
 model = RandomForestClassifier()
 
 # Treinamento dos dados
 training = model.fit(X_train, y_train)
 
-# Predict dos dados
+# Previsão dos dados
 y_preds = model.predict(X_test)
-print(y_preds)
+print(y_preds[:10]) # para cada linha, vai mostrar se é 0 ou 1
+
+print('-----------------------')
+
+y_proba = model.predict_proba(X_test)
+print(y_proba[:10]) # mostra as 10 primeiras linhas com as possibilidades de ser 0 ou 1
+print(y_proba[:10, 1]) # o 1 significa que que vai mostrar as 10 possibilidades de ser 1
+
+print('-----------------------')
+
+### Diferença de 'predict' X 'predict_proba'
+# A principal diferença do 'predict' para o 'predict_proba' é que o predict vai mostrar 0 ou 1
+# , ou seja, para cada linha mostrar se vai ser 0 ou 1.
+# Já o 'predict_proba' vai mostrar, para cada linha, quais as probabilidades de ser 0 ou 1, ou seja, 
+# não escolhe um direto.
+
 
 # Avaliação do modelo
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_score # modelo para diversas avaliações
 from sklearn.metrics import accuracy_score # Checar a acurácia do modelo, ou seja, porcentagem de acertos em cima dos exemplos
 
-default_cross_score = cross_val_score(model, X, y, cv=10, scoring=None) # array de 10 treinamentos diferentes
+default_cross_score = cross_val_score(model, X, y, cv=10, scoring=None) # array de 10 treinamentos diferentes (vai retornar a acurácia padrão (score))
 
-accuracy_cross_score = cross_val_score(model, X, y, cv=10, scoring='accuracy') # array de 10 treinamentos diferentes
+accuracy_cross_score = cross_val_score(model, X, y, cv=10, scoring='accuracy') # array de 10 treinamentos diferentes (vai retornar a acurácia padrão (score))
 
-test_score = accuracy_score(y_test, y_preds) # valor de um treinamento
+test_score = accuracy_score(y_test, y_preds) # vai retornar a acurácia padrão (score)
 
 # São equivalentes ao mesmo resultado.
+
+from sklearn.metrics import roc_curve # distingue as duas classes (0 ou 1) 
+
+# Calculo
+fpr, tpr, thresholds = roc_curve(y_test, y_proba[:, 1])
+# fpr - alarme falso
+# tpr - acerto nos positivos
+# thresholds - limiar de probabilidade usado para calcular 0 ou 1
+
+print(fpr)
+print('-----------------------')
+print(tpr)
+print('-----------------------')
+print(thresholds)
+
+# Gráfico da curva
+plt.plot(fpr, tpr, color='red', label='ROC')
+
+# Personalizando a plotagem
+plt.xlabel("FPR")
+plt.ylabel("TPR")
+plt.title("ROC")
+plt.legend()
+plt.show()
+
+# Avaliação do modelo 'roc_auc_score'
+from sklearn.metrics import roc_auc_score
+
+# Calculo
+auc_roc_score = roc_auc_score(y_test, y_proba[:, 1])
+print(auc_roc_score)
