fb127c4d049f5ce4c06ead1b5f33447c5b470f00
HeitorDalla <heitorvillavilla@gmail.com>
2025-07-04 21:15:54 -0300

    Finalizado um modelo de machine learning aplicando o RandomizedSearchCV para parametrizacao do modelo

---
+++ hiperparameters/randomized_search_CV.py
@@ -0,0 +1,86 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
+
+# Parâmetros de Utilização
+
+## max_depth
+## max_features
+## min_samples_leaf
+## min_samples_split
+## n_estimators
+
+# Função para retornar a avaliação do modelo por meio de Métricas
+def evaluated_metrics (y_true, y_preds):
+    accuracy = accuracy_score(y_true, y_preds)
+    f1 = f1_score(y_true, y_preds)
+    precision = precision_score(y_true, y_preds)
+    recall = recall_score(y_true, y_preds)
+
+    metrics = {
+        'accuracy': round(accuracy, 2),
+        'f1': round(f1, 2),
+        'precision': round(precision, 2),
+        'recall' : round(recall, 2)
+    }
+
+    print(f"Acc: {accuracy * 100:.2f}%")
+    print(f"Precision: {precision:.2f}")
+    print(f"Recall: {recall:.2f}")
+    print(f"F1 score: {f1:.2f}")
+
+    return metrics
+
+
+# Tratamento dos Dados
+carregamento = load_breast_cancer()
+
+dataframe = pd.DataFrame(carregamento['data'], columns=carregamento['feature_names'])
+
+dataframe['target'] = carregamento['target']
+
+X = dataframe.drop('target', axis=1)
+y = dataframe['target']
+
+# Separar os dados em treinamento e teste
+X_train, X_test, y_train, y_test = train_test_split(X,
+                                                    y,
+                                                    test_size=0.2,
+                                                    random_state=42
+                                                    )
+
+from sklearn.model_selection import RandomizedSearchCV
+
+# Instanciar o Modelo à ser usado
+model = RandomForestClassifier(n_jobs=1, random_state=42) # quantas CPUs o algorítmo vai utilizar para realizar as tarefas
+
+grid = {
+    "max_depth": [None, 5, 10, 20, 30],
+    "max_features": ['sqrt', 'log2'],
+    "min_samples_leaf": [1, 2, 4],
+    "min_samples_split": [2, 4, 6],
+    "n_estimators": [10, 100, 200, 500]
+}
+
+rs_model = RandomizedSearchCV(model, # modelo que vai utilizar
+                              param_distributions=grid, # os parâmetros que vai utilizar
+                              n_iter=10,
+                              cv=5,
+                              verbose=2)
+
+modelo_parametrizado = rs_model.fit(X_train, y_train)
+print(modelo_parametrizado)
+
+# Ver o melhor resultado da parametrização
+print(rs_model.best_params_)
+
+# Fazer previsões com o modelo parametrizado
+rs_y_preds = rs_model.predict(X_test)
+
+# Classificar o modelo com as métricas
+rs_metrics = evaluated_metrics(y_test, rs_y_preds)
+print(rs_metrics)
